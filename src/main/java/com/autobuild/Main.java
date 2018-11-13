283df6bc7bf2b30af2e02487c550d86aa5749e1e
dexuefu <fdexue@gmail.com>
2018-11-13 23:57:49 +0800

    项目搭建

---
+++ src/main/java/com/autobuild/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018. 浙江车国网络科技有限公司.  All rights reserved.
+ */
+package com.autobuild;
+
+/**
+ * Created by fdx on 18/11/13.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hel");
+    }
+}
